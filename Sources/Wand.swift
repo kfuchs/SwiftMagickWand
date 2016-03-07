dc1abd5824538eac88df0d6077a15fe69beea87f
Yusuke Ito <novi.mad@gmail.com>
2016-03-07 18:59:56 +0900

    implement resize func

--- Sources/Wand.swift
+++ Sources/Wand.swift
@@ -8,3 +8,35 @@
 
 import MagickWand
 
+public class Wand {
+    
+    public typealias BufferType = UInt8
+    
+    public static func genesis() {
+        MagickWandGenesis()
+    }
+    
+    let wand: COpaquePointer
+    
+    init() {
+        wand = NewMagickWand()
+    }
+    public convenience init(imageBytes: [BufferType]) {
+        self.init()
+        MagickReadImageBlob(wand, imageBytes, imageBytes.count)
+    }
+    
+    public func resize(w: Int, h: Int) {
+        MagickResizeImage(wand, w, h, LanczosFilter, 1.0)
+    }
+    
+    public func getImageBytes() -> [BufferType] {
+        var size: Int = 0
+        let mem = MagickWriteImageBlob(wand, &size)
+        var outBuf = Array<BufferType>.init(count: size, repeatedValue: 0)
+        for i in 0..<size {
+            outBuf[i] = mem[i]
+        }
+        return outBuf
+    }
+}
